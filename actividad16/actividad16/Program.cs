856eef00d1ed7edfced5c89cd3c0735d848f9be6
ALUMNO <ALUMNO@DESKTOP-KD6Q3JT>
2026-03-20 08:18:23 -0600

    Creación del menú y solicitar al usuario que ingrese una opción. Verificar que el usuario haya ingresado un número

--- actividad16/actividad16/Program.cs
+++ actividad16/actividad16/Program.cs
@@ -1,6 +1,24 @@
-﻿class Producto
+﻿Dictionary<int,Producto > productos = new Dictionary<int,Producto>();
+bool continuar;
+int opcion;
+do
+{
+    Console.WriteLine("Bienvenido, seleccione una opción");
+    Console.WriteLine("1) Agregar producto");
+    Console.WriteLine("2) Modificar producto");
+    Console.WriteLine("3) Eliminar producto");
+    Console.WriteLine("4) Buscar producto");
+    Console.WriteLine("5) Mostrar todos los productos");
+    Console.WriteLine("6) Salir");
+    continuar = int.TryParse(Console.ReadLine(), out opcion);
+} while (opcion!=6);
+class Producto
 {
     public string nombre;
     public double precio;
-    public int codigo;
+    public void MostrarInfo()
+    {
+        Console.WriteLine($"Nombre del producto: {nombre}");
+        Console.WriteLine($"Precio: {precio}\n");
+    }
 }
