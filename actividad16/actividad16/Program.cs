9d75afa16ae52cc2eaa00fcc6f4b0023dc89b490
ALUMNO <ALUMNO@DESKTOP-KD6Q3JT>
2026-03-20 08:29:58 -0600

    Creación del case 1 Agregar producto, que solicita datos al usuario y verifica que hayan sido ingresados antes y sean válidos

--- actividad16/actividad16/Program.cs
+++ actividad16/actividad16/Program.cs
@@ -1,6 +1,6 @@
 ﻿Dictionary<int,Producto > productos = new Dictionary<int,Producto>();
 bool continuar;
-int opcion;
+int opcion, codigo;
 do
 {
     Console.WriteLine("Bienvenido, seleccione una opción");
@@ -11,6 +11,58 @@ do
     Console.WriteLine("5) Mostrar todos los productos");
     Console.WriteLine("6) Salir");
     continuar = int.TryParse(Console.ReadLine(), out opcion);
+    Console.Clear();
+    if (continuar && (opcion>=1 && opcion<=6))
+    {
+        switch (opcion)
+        {
+            case 1:
+                
+                do
+                {
+                    Console.WriteLine("Ingrese el código del producto");
+                    continuar= int.TryParse(Console.ReadLine(), out codigo);
+                    if (continuar)
+                    {
+                        if (productos.ContainsKey(codigo))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("El codigo ingresado ya existe");
+                        }
+                        else
+                        {
+                            Producto p = new Producto();
+                            Console.WriteLine("Ingrese el nombre del producto");
+                            p.nombre = Console.ReadLine();
+                            Console.WriteLine("Ingrese el precio del producto");
+                            continuar = double.TryParse(Console.ReadLine(), out p.precio);
+                            if(continuar && p.precio > 0)
+                            {
+                                Console.Clear();
+                                productos.Add(codigo, p);
+                                Console.WriteLine("El producto se ha agregado correctamente");
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Asegurese de ingresar un precio válido");
+                                continuar = false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Asegurese de ingresar solo números enteros");
+                    }
+                } while (continuar==false);
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Opción ingresada inválida");
+    }
 } while (opcion!=6);
 class Producto
 {
